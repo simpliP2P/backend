6249ada61675634590d63396740ede9c0313a53a
0xaplus <codewithaplus@gmail.com>
2025-01-10 17:44:34 +0100

    fix: add logger to providers

--- src/Modules/Token/token.module.ts
+++ src/Modules/Token/token.module.ts
@@ -4,11 +4,12 @@ import { Token } from "./Entities/token.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TokenService } from "./Services/token.service";
 import { JwtModule } from "@nestjs/jwt";
+import { AppLogger } from "src/Logger/logger.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Token]), JwtModule.register({})],
   controllers: [],
-  providers: [TokenHelper, TokenService],
+  providers: [TokenHelper, TokenService, AppLogger],
   exports: [TokenHelper, TokenService],
 })
 export class TokenModule {}
