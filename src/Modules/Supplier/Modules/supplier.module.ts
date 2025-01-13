fe3ce00be13df1b8d704303b605ffd1b373f96c1
Abdulsalam Adams <codewithaplus@gmail.com>
2025-01-13 18:46:37 +0100

    Update supplier.module.ts

--- src/Modules/Supplier/Modules/supplier.module.ts
+++ src/Modules/Supplier/Modules/supplier.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { SuppliersController } from "../Controllers/supplier.controller";
+// import { SuppliersController } from "../Controllers/supplier.controller";
 import { SuppliersService } from "../Services/supplier.service";
 import { Supplier } from "../Entities/supplier.entity";
 import { UserOrganisationRepository } from "src/Modules/Organisation/Repositories/userOrganisation.repository";
