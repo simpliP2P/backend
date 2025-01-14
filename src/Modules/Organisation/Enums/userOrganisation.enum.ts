8a4a44dd4a914eda0a5d0472364fd34706d2bf73
0xaplus <codewithaplus@gmail.com>
2025-01-14 01:59:33 +0100

    add manage purchase requisitions

--- src/Modules/Organisation/Enums/userOrganisation.enum.ts
+++ src/Modules/Organisation/Enums/userOrganisation.enum.ts
@@ -2,4 +2,5 @@ export enum PermissionType {
     OWNER = "all_permissions",
     MANAGE_USERS = "manage_users",
     MANAGE_SUPPLIERS = "manage_suppliers",
+    MANAGE_PURCHASE_REQUISITIONS = "manage_purchase_requisitions",
 }
