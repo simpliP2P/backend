c4a2e273f7622b0b179e228e4a8adae79295cd33
0xaplus <codewithaplus@gmail.com>
2025-01-31 12:55:36 +0100

    feat(PurchaseRequisition): saved requisitions

--- src/Modules/PurchaseRequisition/Services/purchaseRequisition.service.ts
+++ src/Modules/PurchaseRequisition/Services/purchaseRequisition.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { Not, Repository } from "typeorm";
 import { PurchaseRequisition } from "../Entities/purchaseRequisition.entity";
-import { Organisation } from "src/Modules/Organisation/Entities/organisation.entity";
 import { PurchaseRequisitionStatus } from "../Enums/purchaseRequisition.enum";
 
 @Injectable()
@@ -10,28 +9,71 @@ export class PurchaseRequisitionService {
   constructor(
     @InjectRepository(PurchaseRequisition)
     private readonly purchaseRequisitionRepository: Repository<PurchaseRequisition>,
-    @InjectRepository(Organisation)
-    private readonly organisationRepository: Repository<Organisation>,
   ) {}
 
   public async createPurchaseRequisition(
     organisationId: string,
     data: Partial<PurchaseRequisition>,
   ): Promise<PurchaseRequisition> {
-    const organisation = await this.organisationRepository.findOne({
-      where: { id: organisationId },
-    });
-
-    if (!organisation) {
-      throw new NotFoundException("Organisation not found");
-    }
-
     const newRequisition = this.purchaseRequisitionRepository.create({
       ...data,
-      organisation,
+      organisation: { id: organisationId },
     });
 
     return this.purchaseRequisitionRepository.save(newRequisition);
+  }
+
+  public async getAllPurchaseRequisitions(
+    page: number = 1,
+    pageSize: number = 10,
+    userId: string,
+    organisationId: string,
+  ) {
+    let _page = page;
+    let _pageSize = pageSize;
+    if (isNaN(page) || page < 1) _page = 1;
+    if (isNaN(pageSize) || pageSize < 1) _pageSize = 10;
+
+    const skip = (_page - 1) * _pageSize; // Calculate the offset
+
+    const requisitions = await this.getPurchaseRequisitions({
+      where: {
+        created_by: { id: userId },
+        organisation: { id: organisationId },
+        status: Not(PurchaseRequisitionStatus.SAVED_FOR_LATER),
+      },
+      take: _pageSize,
+      skip,
+      relations: ["created_by"],
+      select: {
+        created_by: {
+          first_name: true,
+          id: true,
+        },
+      },
+    });
+
+    return requisitions;
+  }
+
+  public async getPurchaseRequisitionById(
+    organisationId: string,
+    requisitionId: string,
+  ): Promise<PurchaseRequisition | null> {
+    return await this.purchaseRequisitionRepository.findOne({
+      where: {
+        organisation: { id: organisationId },
+        id: requisitionId,
+        status: Not(PurchaseRequisitionStatus.SAVED_FOR_LATER),
+      },
+      relations: ["created_by"],
+      select: {
+        created_by: {
+          first_name: true,
+          id: true,
+        },
+      },
+    });
   }
 
   public async updateApprovalDetails(
@@ -59,16 +101,29 @@ export class PurchaseRequisitionService {
 
   // returns: purchase requisition saved for later
   public async getSavedPurchaseRequisitions(
+    page: number = 1,
+    pageSize: number = 10,
     userId: string,
     organisationId: string,
   ) {
-    return await this.getPurchaseRequisitions({
+    let _page = page;
+    let _pageSize = pageSize;
+    if (isNaN(page) || page < 1) _page = 1;
+    if (isNaN(pageSize) || pageSize < 1) _pageSize = 10;
+
+    const skip = (_page - 1) * _pageSize; // Calculate the offset
+
+    const requisitions = await this.getPurchaseRequisitions({
       where: {
-        created_by: userId,
-        organisation_id: organisationId,
+        created_by: { id: userId },
+        organisation: { id: organisationId },
         status: PurchaseRequisitionStatus.SAVED_FOR_LATER,
       },
+      take: _pageSize,
+      skip,
     });
+
+    return requisitions;
   }
 
   public async getPurchaseRequisitions(
