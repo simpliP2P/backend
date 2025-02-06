6015e264042ea80b0aaf030a0c37fb59b3d86677
0xaplus <codewithaplus@gmail.com>
2025-02-06 02:19:39 +0100

    generateHashFromId

--- src/Modules/Organisation/Services/organisation.service.ts
+++ src/Modules/Organisation/Services/organisation.service.ts
@@ -29,6 +29,7 @@ import { SuppliersService } from "src/Modules/Supplier/Services/supplier.service
 import { ProductService } from "src/Modules/Product/Services/product.service";
 import { UploadService } from "src/Modules/Upload/Services/upload.service";
 import { ConfigService } from "@nestjs/config";
+import { createHash } from "crypto";
 
 @Injectable()
 export class OrganisationService {
@@ -369,6 +370,11 @@ export class OrganisationService {
       user: { id: userId },
       organisation: { id: organisationId },
     });
+  }
+
+  public generateHashFromId(id: string): string {
+    const hash = createHash("sha256").update(id).digest("hex");
+    return parseInt(hash.substring(0, 10), 16).toString(36).substring(0, 8); // Convert hex to base36
   }
 
   private generateStrongPassword(): string {
