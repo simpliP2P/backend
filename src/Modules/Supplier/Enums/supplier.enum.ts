aaadd433015118855cc112ba82570e6e038a38e8
0xaplus <codewithaplus@gmail.com>
2025-04-14 14:52:08 +0100

    update

--- src/Modules/Supplier/Enums/supplier.enum.ts
+++ src/Modules/Supplier/Enums/supplier.enum.ts
@@ -1,14 +1,14 @@
 export enum PaymentTerms {
-  PIA = "Payment in Advance",
-  COD = "Cash on Delivery",
-  LOC = "Line of Credit",
-  NT00 = "Payment Immediately",
-  NT15 = "15 days payment after invoice",
-  NT30 = "30 days payment after invoice",
-  NT45 = "45 days payment after invoice",
-  NT60 = "60 days payment after invoice",
-  NT90 = "90 days payment after invoice",
-  NT120 = "120 days payment after invoice",
+  PIA = "pia", // "Payment in Advance"
+  COD = "cod", // "Cash on Delivery"
+  LOC = "loc", // "Line of Credit"
+  NT00 = "nt00", // "Payment Immediately"
+  NT15 = "nt15", // "15 days payment after invoice"
+  NT30 = "nt30", // "30 days payment after invoice"
+  NT45 = "nt45", // "45 days payment after invoice"
+  NT60 = "nt60", // "60 days payment after invoice"
+  NT90 = "nt90", // "90 days payment after invoice"
+  NT120 = "nt120", // "120 days payment after invoice"
 }
 
 export enum NotificationChannels {
