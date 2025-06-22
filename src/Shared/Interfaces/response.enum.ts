5e033152e6c74ae8ec4b75cf72762c1193b0d509
0xaplus <codewithaplus@gmail.com>
2025-06-22 22:05:49 +0100

    response enums

---
+++ src/Shared/Interfaces/response.enum.ts
@@ -0,0 +1,14 @@
+export enum RESPONSE_STATUS_CODE {
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  OK = 200,
+  CREATED = 201,
+  SERVER_ERROR = 500,
+}
+
+export enum RESPONSE_STATUS {
+  SUCCESS = "success",
+  ERROR = "error",
+}
+  
