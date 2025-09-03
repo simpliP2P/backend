5ff2ab7dc025346829f59238d8763c338a6f5c38
0xaplus <codewithaplus@gmail.com>
2025-09-03 15:53:14 +0100

    refactor: update budget module imports to include OrganisationStructureModule and OrganisationAccessModule

--- src/Modules/Budget/Modules/budget.module.ts
+++ src/Modules/Budget/Modules/budget.module.ts
@@ -3,10 +3,15 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Budget } from "../Entities/budget.entity";
 import { BudgetService } from "../Services/budget.service";
 import { BudgetController } from "../Controllers/budget.controller";
-import { OrganisationModule } from "src/Modules/Organisation/Modules/organisation.module";
+import { OrganisationStructureModule } from "src/Modules/Organisation/Modules/organisation-structure.module";
+import { OrganisationAccessModule } from "src/Modules/Organisation/Modules/organisation-access.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Budget]), OrganisationModule],
+  imports: [
+    TypeOrmModule.forFeature([Budget]),
+    OrganisationStructureModule,
+    OrganisationAccessModule,
+  ],
   providers: [BudgetService],
   controllers: [BudgetController],
   exports: [BudgetService],
