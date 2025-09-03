7a7168da87857ddc19308d9cdd29b8dfc1deb430
0xaplus <codewithaplus@gmail.com>
2025-09-03 17:13:59 +0100

    feat: update SharedModule to include PerformanceHelper for enhanced functionality

--- src/Shared/shared.module.ts
+++ src/Shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ClientHelper } from "./Helpers/client.helper";
+import { PerformanceHelper } from "./Helpers/performance.helper";
 
 @Module({
   imports: [ConfigModule],
-  providers: [ClientHelper],
-  exports: [ClientHelper, ConfigModule],
+  providers: [ClientHelper, PerformanceHelper],
+  exports: [ClientHelper, ConfigModule, PerformanceHelper],
 })
 export class SharedModule {}
